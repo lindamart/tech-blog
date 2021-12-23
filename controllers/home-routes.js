f9075efd911774dc825235857c929aec9e4eab8d
Linda Martinez <lindanmjw@gmail.com>
2021-12-23 09:24:45 -0500

    added dashboard.handlebars and updated home-routs.js with render code

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
 
-router.get("/signup", (req, res)=>{
-    if(req.session.loggedIn){
+router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
         res.redirect("/")
         return
     }
     res.render("signup")
+
 })
 
 router.get("/login", (req, res) => {
     res.render('login')
 });
+
+// do i put this into above code?
+res.render('homepage', {
+//    what do i pass here? logged in status? logged_in: req.session.logged_in 
+});
+catch (err) {
+    res.status(500).json(err);
+}
 
 module.exports = router;
