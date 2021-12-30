95a9fba651b6df4bbd44650d1f9d2646b1a322a8
Linda Martinez <lindanmjw@gmail.com>
2021-12-29 21:45:50 -0500

    adding code for login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,31 @@
 // connect this file to your login.handlebars
 
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
 // reference signup.js to send data to backend (user-routes.js) from here
 
 // once user login routes are working properly get them to render ONLY dashboard.handlebars (not main and dashboard)
