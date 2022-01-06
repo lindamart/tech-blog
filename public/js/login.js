b1ac47ac19cb56befc015be6899c517ddae9b799
Linda Martinez <lindanmjw@gmail.com>
2022-01-05 21:24:42 -0500

    correcting user id

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
     if (username && password) {
